5d6d217d7e24f1fb04c746bfef113f3263dd1025
Teodor Profeanu <teodor.profeanu@gmail.com>
2023-03-17 04:12:51 +0200

    first commit

---
+++ src/main/java/com/example/gamestoreapi/GameStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamestoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamestoreapi/GameStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
